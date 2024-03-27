55ff75af799f07b606db1ef7f57c421626185198
Harry San Juan <harrycsanjuan@gmail.com>
2024-03-27 17:09:36 -0400

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
+# def print_fibonacci(length):
+#     if length == 0:
+#         fibonacci_sequence.clear()
+#     elif length == 1:
+#         fibonacci_sequence.pop()
+#     else:
+#         fibonacci_sequence = [0, 1]
+#         while len(fibonacci_sequence) < length:
+#             next_sequence = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+#             fibonacci_sequence.append(next_sequence)
+
+#     print(fibonacci_sequence)
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fibonacci_sequence = [0, 1]
+        while len(fibonacci_sequence) < length:
+            next_sequence = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_sequence)
+        print(fibonacci_sequence)
